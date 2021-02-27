510135a06d50e7890f9905b89f08a82d40d24300
Robotics <robot@usd373.org>
2021-02-27 14:29:28 -0600

    changed map bound widget to test target rectangle rendering

--- src/main/java/edu/team935/MyPlugin/MyMapBoundWidget.java
+++ src/main/java/edu/team935/MyPlugin/MyMapBoundWidget.java
@@ -5,13 +5,13 @@ import edu.wpi.first.shuffleboard.api.data.types.MapType;
 import edu.wpi.first.shuffleboard.api.widget.Description;
 import edu.wpi.first.shuffleboard.api.widget.ParametrizedController;
 import edu.wpi.first.shuffleboard.api.widget.SimpleAnnotatedWidget;
-import eu.hansolo.medusa.Gauge;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
 import edu.wpi.first.shuffleboard.api.prefs.Group;
 import edu.wpi.first.shuffleboard.api.prefs.Setting;
 import java.util.LinkedList;
@@ -24,19 +24,13 @@ public class MyMapBoundWidget extends SimpleAnnotatedWidget<MapData> implements 
 	private AnchorPane _thePane;
 
 	@FXML
-	private Gauge _topGauge;
+	private Rectangle _target;
 
-	@FXML
-	private Gauge _middleGauge;
-
-	@FXML
-	private Gauge _bottomGauge;
-
-	private final SimpleStringProperty _topKey = new SimpleStringProperty(this, "topGaugeKey", "");
-	private final SimpleStringProperty _middleKey = new SimpleStringProperty(this, "middleGaugeKey", "");
-	private final SimpleStringProperty _bottomKey = new SimpleStringProperty(this, "bottomGaugeKey", "");
-	
-	
+	private final SimpleStringProperty _targetXKey = new SimpleStringProperty(this, "targetX", "");
+	private final SimpleStringProperty _targetYKey = new SimpleStringProperty(this, "targetY", "");
+	private final SimpleStringProperty _targetWKey = new SimpleStringProperty(this, "targetW", "");
+	private final SimpleStringProperty _targetHKey = new SimpleStringProperty(this, "targetH", "");
+		
 	public MyMapBoundWidget ()
 	{
 		// Export the properties to set the key value to bind to each gauge.
@@ -45,37 +39,46 @@ public class MyMapBoundWidget extends SimpleAnnotatedWidget<MapData> implements 
 		dataProperty().addListener(this);
 	}
 
-	
-	public String getTopGaugeKey ()
+	public String getTargetXKey ()
 	{
-		return _topKey.getValue();
+		return _targetXKey.getValue();
 	}
 	
-	public void setTopGaugeKey (String top)
+	public void setTargetXKey (String targetX)
 	{
-		_topKey.setValue(top);
+		_targetXKey.setValue(targetX);
 	}
 	
-	public String getMiddleGaugeKey ()
+	public String getTargetYKey ()
 	{
-		return _middleKey.getValue();
+		return _targetYKey.getValue();
 	}
 	
-	public void setMiddleGaugeKey (String middle)
+	public void setTargetYKey (String targetY)
 	{
-		_middleKey.setValue(middle);
+		_targetYKey.setValue(targetY);
 	}
 	
-
-	public String getBottomGaugeKey ()
+	public String getTargetWKey ()
 	{
-		return _bottomKey.getValue();
+		return _targetWKey.getValue();
 	}
 	
-	public void setBottomGaugeKey (String bottom)
+	public void setTargetWKey (String targetW)
 	{
-		_bottomKey.setValue(bottom);
+		_targetWKey.setValue(targetW);
 	}
+	
+	public String getTargetHKey ()
+	{
+		return _targetHKey.getValue();
+	}
+	
+	public void setTargetHKey (String targetH)
+	{
+		_targetHKey.setValue(targetH);
+	}
+	
 	
 	@Override
 	public java.util.List<edu.wpi.first.shuffleboard.api.prefs.Group> getSettings() {
@@ -83,11 +86,11 @@ public class MyMapBoundWidget extends SimpleAnnotatedWidget<MapData> implements 
 		LinkedList<Group> propertyList = new LinkedList<Group>();
 
 		propertyList.add(Group.of("Map Key Values"
-		, Setting.of("Top Key", "The key value in the map to assign to the top gauge.", _topKey, String.class)
-		, Setting.of("Middle Key", "The key value in the map to assign to the middle gauge.", _middleKey, String.class)
-		, Setting.of("Bottom Key", "The key value in the map to assign to the bottom gauge.", _bottomKey, String.class)
+		, Setting.of("Target X Key", "The key value in the map to assign to the target X.", _targetXKey, String.class)
+		, Setting.of("Target Y Key", "The key value in the map to assign to the target Y.", _targetYKey, String.class)
+		, Setting.of("Target W Key", "The key value in the map to assign to the target W.", _targetWKey, String.class)
+		, Setting.of("Target H Key", "The key value in the map to assign to the target H.", _targetHKey, String.class)
 		));
-
 
 		return propertyList;
 	  }
@@ -107,22 +110,34 @@ public class MyMapBoundWidget extends SimpleAnnotatedWidget<MapData> implements 
 	 * @param g The gauge control to set the value in.
 	 * @param key The key value from the map to find the data to set in the gauge.
 	 */
-	private void setValue (Gauge g, String key)
-	{
-		if (key != null && !key.isEmpty() && dataProperty().get().get(key) != null)
-			g.valueProperty().set((double) dataProperty().get().get(key));
-	}
+//	private void setValue (Gauge g, String key)
+//	{
+//		if (key != null && !key.isEmpty() && dataProperty().get().get(key) != null)
+//			g.valueProperty().set((double) dataProperty().get().get(key));
+//	}
 	
 	@Override
 	public void changed(ObservableValue<? extends MapData> arg0, MapData arg1, MapData arg2) {
 		
 		// Invoked when the map changes.  It gets the value from the map by the key value,
-		// then updates the appropriate gauge.
-		
-		setValue (_topGauge, _topKey.getValue());
-		setValue (_middleGauge, _middleKey.getValue());
-		setValue (_bottomGauge, _bottomKey.getValue());
+		// then updates the appropriate parameter of the rectangle.
 
+		if (_targetXKey.getValue() != null && !_targetXKey.getValue().isEmpty() && dataProperty().get().get(_targetXKey.getValue()) != null)
+			_target.setX((double) dataProperty().get().get(_targetXKey.getValue()));
+
+		if (_targetYKey.getValue() != null && !_targetYKey.getValue().isEmpty() && dataProperty().get().get(_targetYKey.getValue()) != null)
+			_target.setY((double) dataProperty().get().get(_targetYKey.getValue()));
+
+		if (_targetWKey.getValue() != null && !_targetWKey.getValue().isEmpty() && dataProperty().get().get(_targetWKey.getValue()) != null)
+			_target.setWidth((double) dataProperty().get().get(_targetWKey.getValue()));
+
+		if (_targetHKey.getValue() != null && !_targetHKey.getValue().isEmpty() && dataProperty().get().get(_targetHKey.getValue()) != null)
+			_target.setHeight((double) dataProperty().get().get(_targetHKey.getValue()));
+
+//		_target.setX(Double.parseDouble(_targetXKey.getValue()));
+//		_target.setY(Double.parseDouble(_targetYKey.getValue()));
+//		_target.setWidth(Double.parseDouble(_targetWKey.getValue()));
+//		_target.setHeight(Double.parseDouble(_targetHKey.getValue()));
 	}
 
 }
